ca1ed52bf5e888f1dae4bbaf08d0d7d3bcabd6d7
Wilmer <wilmer0593@gmail.com>
2015-01-19 01:11:10 -0430

    listo los 3 problemas, ya probados

--- 2.cpp
+++ 2.cpp
@@ -4,9 +4,7 @@ using namespace std;
 vector<int> padre;
 vector<int> cnt;
 vector<pair<int,int> > arcos;
-vector<vector<int> > next;
 vector<bool> eliminado;
-vector<bool> visitado;
 int casos;
 int N,C;
 stack<int> s_1,s_2;
@@ -31,50 +29,39 @@ int unir(int nodo_1, int nodo_2){
 void inicializar(){
   padre.resize(N);
   cnt.resize(N);
-  
-  next.clear();
-  next.resize(N);
     
-  visitado.clear();
-  visitado.resize(N,false);
-  eliminado.clear();
-  arcos.clear();
-}
-
-void dfs(int nodo, int parent){
-  padre[nodo]=padre[parent];
-  cnt[padre[nodo]]++;
-  visitado[nodo]=1;
-  for(int i=0;i<next[nodo].size();i++){
-    if(parent!=next[nodo][i])
-      dfs(next[nodo][i],nodo);
-  }
-  
+  eliminado.resize(N-1);
+  arcos.resize(N-1);
 }
 
 int main(){
-  cin >> casos;
+  scanf("%d",&casos);
   while(casos--){
-    cin >> N;
+    scanf("%d",&N);
     
     inicializar();
     
     for(int i=0;i<N-1;i++){
       int nod_1,nod_2;
-      cin >> nod_1 >> nod_2;
+      scanf("%d %d",&nod_1,&nod_2);
       nod_1--;
       nod_2--;
-      arcos.push_back(make_pair(nod_1,nod_2));
-      eliminado.push_back(false);
+      arcos[i]=make_pair(nod_1,nod_2);
+      eliminado[i]=false;
+      
+      padre[i]=i;
+      cnt[i]=1;
     }
+    padre[N-1]=N-1;
+    cnt[N-1]=1;
     
-    cin >> C;
+    scanf("%d",&C);
     char tmp[4];
     int eliminar;
     for(int i=0;i<C;i++){
-      cin >> tmp;
+      scanf("%s",tmp);
       if(tmp[0]=='R'){
-        cin >> eliminar;
+        scanf("%d",&eliminar);
         eliminar--;
         eliminado[eliminar]=true;
         s_1.push(1);
@@ -86,25 +73,13 @@ int main(){
     
     for(int i=0;i<N-1;i++){
       if(!eliminado[i]){
-        next[arcos[i].first].push_back(arcos[i].second);
-        next[arcos[i].second].push_back(arcos[i].first);        
+         s_1.push(1);
+         s_2.push(i);
       }
     }
     
-    int total = 0;
+    int total = (N*(N-1))/2;
     int sum = 0;
-    for(int i=0;i<N;i++){
-      if(!visitado[i]){
-        cnt[i]=0;
-        padre[i]=i;
-        dfs(i,i);
-        //cout << "i= " << i << " " << cnt[i] << endl;
-        total+=sum*cnt[i];
-        sum+=cnt[i];
-      }
-    }
-    //cout << "sum= " << sum << endl;
-    //cout << "total= " << total << endl;
     
     vector<int> respuesta;
     
